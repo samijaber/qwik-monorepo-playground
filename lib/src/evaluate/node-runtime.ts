856f33873205533ece8dbc41b1052200c3fe3691
Sami Jaber <jabersami@gmail.com>
2023-11-08 17:47:29 -0400

    more tests
    f

--- lib/src/evaluate/node-runtime.ts
+++ lib/src/evaluate/node-runtime.ts
@@ -1,9 +1,9 @@
-import * as ivm from "isolated-vm";
+import ivm from "isolated-vm";
 
 export const evaluator = (code: string) => {
   const isolate = new ivm.Isolate({ memoryLimit: 128 });
   const context = isolate.createContextSync();
-  const script = isolate.compileScriptSync("1 + 2");
+  const script = isolate.compileScriptSync(code);
   const result = script.runSync(context);
   return result;
 };
