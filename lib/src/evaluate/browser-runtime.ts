e7c0da23ff4da6eae9adf7b85afeaf613702b6ab
Sami Jaber <jabersami@gmail.com>
2023-11-10 10:43:25 -0400

    tmp

--- lib/src/evaluate/browser-runtime.ts
+++
@@ -1,3 +0,0 @@
-export const evaluator = (code: string) => {
-  return new Function("return " + code)();
-};
--- lib/src/evaluate/placeholder-runtime.ts
+++
@@ -1,5 +0,0 @@
-export const evaluator = (code: string) => {
-  throw new Error(
-    "Placeholder evaluator not implemented. Make sure the SDK is properly replacing the import to the evaluator with one of edge/node/browser runtime evaluators."
-  );
-};
